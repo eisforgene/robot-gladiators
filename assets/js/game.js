4a597a4022d8856aa171f1e610b794bc1c99232c
Bomb Atomically <ieugenelee@gmail.com>
2021-06-28 23:11:16 -0700

    fixed name prompt bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -54,7 +54,6 @@ var startGame = function() {
   for (var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1)); // Let player know what round they're in
-      
       var pickedEnemyObj = enemyInfo[i]; // Pick new enemy to fight based on the index of the enemy.names array
      
       pickedEnemyObj.health = randomNumber(40, 60); // Reset enemy health before starting a new fight
@@ -122,8 +121,19 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;  
+}
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
